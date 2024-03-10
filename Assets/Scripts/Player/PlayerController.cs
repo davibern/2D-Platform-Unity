465f0faebee3277a6fbab78268bcc6f855c22999
David Bernabé <david.bern.pal@gmail.com>
2024-03-10 18:25:51 +0100

    Make player jump

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -6,10 +6,17 @@ public class PlayerController : MonoBehaviour
 {
     [Header("Movement")]
     private Vector2 _movement;
-    [SerializeField] private float speed;
+    [SerializeField] private float _speed;
+    [SerializeField] private float _jumpForce;
+    private float _groundCheckRadius = 0.3f;
+    private bool _isGrounded;
 
     [Header("GameObjects")]
     private Rigidbody2D _rb;
+    [SerializeField] private Transform _groundCheck;
+
+    [Header("Layers")]
+    [SerializeField] private LayerMask _groundLayer;
 
     private void Start()
     {
@@ -18,13 +25,26 @@ public class PlayerController : MonoBehaviour
 
     private void Update()
     {
+        // Horizontal movement
         float horizontalInput = Input.GetAxisRaw("Horizontal");
         _movement = new Vector2(horizontalInput, 0f);
+
+        // Check jump
+        _isGrounded = Physics2D.OverlapCircle(_groundCheck.position, _groundCheckRadius, _groundLayer);
+
+        if (Input.GetButtonDown("Jump") && _isGrounded)
+            Jump();
     }
 
     private void FixedUpdate()
     {
-        float horizontalVelocity = _movement.normalized.x * speed;
+        float horizontalVelocity = _movement.normalized.x * _speed;
         _rb.velocity = new Vector2(horizontalVelocity, _rb.velocity.y);
+    }
+
+    private void Jump()
+    {
+        if (!_isGrounded) return;
+        _rb.AddForce(Vector2.up * _jumpForce, ForceMode2D.Impulse);
     }
 }
