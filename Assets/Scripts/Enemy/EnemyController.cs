9c1ca5cde05a69321a690f3c9b872247750101f3
David Bernabé <david.bern.pal@gmail.com>
2024-03-16 18:35:57 +0100

    Triggered animations when slime die

--- Assets/Scripts/Enemy/EnemyController.cs
+++ Assets/Scripts/Enemy/EnemyController.cs
@@ -4,7 +4,18 @@ using UnityEngine;
 
 public class EnemyController : MonoBehaviour
 {
+    [Header("Components")]
+    private Rigidbody2D _rb;
+    private Animator _anim;
+
+    [Header("Live")]
     private float _livingTime = 0.5f;
+
+    private void Awake()
+    {
+        _rb = GetComponent<Rigidbody2D>();
+        _anim = GetComponent<Animator>();
+    }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
@@ -17,9 +28,10 @@ public class EnemyController : MonoBehaviour
     {
         if(collision.gameObject.CompareTag("Player"))
         {
-            Debug.Log("triggered");
-            Destroy(gameObject, _livingTime);
+            _anim.SetTrigger("Die");
             PlayerController.Instance.HitEnemy = true;
+            //gameObject.SetActive(false);
+            Destroy(gameObject, _livingTime);
         }
     }
 }
