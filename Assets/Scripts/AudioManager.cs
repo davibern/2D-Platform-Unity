1fc16202b41ff66181eafe01da287731d2e2daba
David Bernabé <david.bern.pal@gmail.com>
2024-03-17 19:16:04 +0100

    Create AudioManager

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    [Header("Singleton")]
+    private static AudioManager _instance;
+
+    [Header("Components")]
+    [SerializeField] AudioClip _jump;
+    [SerializeField] AudioClip _coin;
+    [SerializeField] AudioClip _gui;
+    [SerializeField] AudioClip _damage;
+    [SerializeField] AudioClip _win;
+    [SerializeField] AudioClip _defeatSlime;
+    private AudioSource _as;
+
+    public static AudioManager Instance {  get { return _instance; } }
+
+    private void Awake()
+    {
+        _as = gameObject.AddComponent<AudioSource>();
+
+        if (_instance != null && _instance != this)
+        {
+            Destroy(this.gameObject);
+        } else
+        {
+            _instance = this;
+        }
+    }
+    private void PlaySound(AudioClip clip)
+    {
+        if (clip != null)
+        {
+            _as.PlayOneShot(clip);
+            _as.volume = 0.5f;
+        }
+    }
+
+    private void OnDestroy()
+    {
+        _instance = null;
+    }
+
+    public void PlayJump() { PlaySound(_jump); }
+    public void PlayCoin() { PlaySound(_coin); }
+    public void PlayGui() { PlaySound(_gui); }
+    public void PlayDefeatSlime() { PlaySound(_defeatSlime); }
+    public void PlayDamage() { PlaySound(_damage); }
+    public void PlayWin() { PlaySound(_win); }
+}
