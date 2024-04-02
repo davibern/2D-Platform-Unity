f3ca0d72ca9b1925d884998d4a764b71fce2bd9f
David Bernabé <david.bern.pal@gmail.com>
2024-04-02 15:56:51 +0200

    Add attack when enemy is close to player

--- Assets/Scripts/Enemy/WayPoint.cs
+++ Assets/Scripts/Enemy/WayPoint.cs
@@ -1,13 +1,15 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class WayPoint : MonoBehaviour
 {
-    [Header("Movmenet")]
+    [Header("Movemenet")]
     [SerializeField] private float _speed;
     [SerializeField] private float _distance;
+    [SerializeField] private bool _isFacingRight;
 
     [Header("TypeMovement")]
     [SerializeField] private bool _isRandomMovement;
@@ -16,6 +18,10 @@ public class WayPoint : MonoBehaviour
     private int _randNumber;
     private SpriteRenderer _sp;
     private Animator _anim;
+
+    [Header("Player")]
+    [SerializeField] private Transform _target;
+    [SerializeField] private float _minDistance;
 
     private void Awake()
     {
@@ -30,7 +36,7 @@ public class WayPoint : MonoBehaviour
         _anim.SetBool("Walk", true);
     }
 
-    private void FixedUpdate()
+    private void Update()
     {
         if (_isRandomMovement)
         {
@@ -47,12 +53,34 @@ public class WayPoint : MonoBehaviour
         {
             transform.position = Vector2.MoveTowards(transform.position, _transform[_nextStep].position, _speed * Time.deltaTime);
 
-            if (Vector2.Distance(transform.position, _transform[_nextStep].position) < _distance )
-            {
-                _nextStep += 1;
+            float distance = Vector2.Distance(transform.position, _target.position);
 
-                if (HasParameter("Walk"))
-                    _anim.SetBool("Walk", true);
+            if (distance < _minDistance && !_isFacingRight)
+            {
+                _anim.SetBool("Attack", true);
+                _anim.SetBool("Walk", false);
+
+                // Obtener el BoxCollider2D del hijo
+                BoxCollider2D childCollider = transform.GetChild(0).GetComponent<BoxCollider2D>();
+                if (!_isFacingRight) // Si el padre no está mirando hacia la derecha
+                {
+                    // Modificar el offset del BoxCollider2D del hijo para moverlo a la izquierda
+                    childCollider.offset = new Vector2(childCollider.offset.x * -1, childCollider.offset.y);
+                    childCollider.isTrigger = true;
+                }
+                else
+                {
+                    // Si el padre está mirando hacia la derecha, restablecer el offset del BoxCollider2D del hijo
+                    childCollider.offset = new Vector2(1.0f, childCollider.offset.y);
+                    childCollider.isTrigger = true;
+                }
+            }
+            else if (Vector2.Distance(transform.position, _transform[_nextStep].position) < _distance )
+            {
+                _anim.SetBool("Attack", false);
+                _anim.SetBool("Walk", true);
+
+                _nextStep += 1;
 
                 if (_nextStep >= _transform.Length )
                     _nextStep = 0;
@@ -80,9 +108,11 @@ public class WayPoint : MonoBehaviour
         if (transform.position.x < _transform[next].position.x)
         {
             _sp.flipX = false;
+            _isFacingRight = true;
         } else
         {
             _sp.flipX = true;
+            _isFacingRight = false;
         }
     }
 
